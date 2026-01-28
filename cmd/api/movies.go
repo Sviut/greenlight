1fd243df10fd9955c2d76f71a851a82b30e7af97
sviut <jeleznov88@gmail.com>
2026-01-28 20:34:33 +0300

    add validator

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 	"greenlight/internal/data"
+	"greenlight/internal/validator"
 	"net/http"
 	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// Объявляем анонимную структуру для хранения информации, которую мы ожидаем
-	// получить в теле HTTP-запроса (обратите внимание, что имена полей и типы
-	// в структуре являются подмножеством структуры Movie, которую мы создали ранее).
-	// Эта структура будет нашей *целевой точкой декодирования*.
 	var input struct {
 		Title   string       `json:"title"`
 		Year    int32        `json:"year"`
@@ -19,19 +16,26 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Genres  []string     `json:"genres"`
 	}
 
-	// Инициализируем новый экземпляр json.Decoder, который читает из тела запроса,
-	// и затем используем метод Decode() для декодирования содержимого тела в структуру input.
-	// Важно: обратите внимание, что при вызове Decode() мы передаем *указатель*
-	// на структуру input. Если во время декодирования произошла ошибка, мы используем
-	// нашу общую вспомогательную функцию errorResponse(), чтобы отправить клиенту
-	// ответ 400 Bad Request с сообщением об ошибке.
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
-	// Выводим содержимое структуры input в HTTP-ответ.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
