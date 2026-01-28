00a4585f4161ff80b40cb97211f219458eaa86d3
sviut <jeleznov88@gmail.com>
2026-01-28 20:21:51 +0300

    UnmarshalJSON Runtime

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -13,10 +13,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// в структуре являются подмножеством структуры Movie, которую мы создали ранее).
 	// Эта структура будет нашей *целевой точкой декодирования*.
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	// Инициализируем новый экземпляр json.Decoder, который читает из тела запроса,
